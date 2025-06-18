816c9a6ff67917497e27be6e6ae98757202a6233
Hanyeojun <hyj78396@naver.com>
2025-06-19 03:06:26 +0900

    Merge Feat/bundleDiscount

--- server.js
+++ server.js
@@ -5,29 +5,28 @@ const app = express()
 //port 번호 설정
 const port = 3000
 
-import cors from 'cors';
-import dotenv from 'dotenv';
-import chatRouter from './routes/chat.js'; 
+import cors from 'cors'
+import dotenv from 'dotenv'
+import chatRouter from './routes/chat.js'
 
-dotenv.config();
+dotenv.config()
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
   })
-);
-app.use(express.json());
+)
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello!')
 })
 
-app.use('/api/gpt', chatRouter);
+app.use('/api/gpt', chatRouter)
 
 //http 서버 실행
 app.listen(port, () => {
   console.log(`Server started... ${port}`)
 })
-
