28703bbf0de5ebe0825d53841d1323e9a9aa09b9
alex8396 <alex8396@naver.com>
2025-06-22 19:23:05 +0900

    feat: users 수정

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcryptjs' // bcryptjs로 통일
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,18 +9,16 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   plan_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Plan', // 여기 중요!
+    ref: 'Plan',
   },
 })
 
-// 저장 전 비밀번호 해싱
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next()
   this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
-// 비밀번호 비교 메서드
 userSchema.methods.comparePassword = function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password)
 }
