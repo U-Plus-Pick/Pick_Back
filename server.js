aee5661e36e88127ef5e74bb0a3bd934b6df5771
박용규 <yoyo5411@naver.com>
2025-06-23 18:53:19 +0900

    Feat : CORS PATCH 추가

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ connectDB()
 app.use(
   cors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 )
