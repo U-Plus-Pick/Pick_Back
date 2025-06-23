81856b1797a17e73e84ad02c7b5027db60ea0412
Hanyeojun <hyj78396@naver.com>
2025-06-23 11:12:16 +0900

    Feat: 전체 요금제 조회 api 수정

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI) // ← 여기를 수정!
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log('MongoDB connected')
   } catch (err) {
     console.error('DB connection error:', err.message)
