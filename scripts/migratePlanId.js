508cf56b91940092f93689b21c7a84cb6b93dd97
alex8396 <alex8396@naver.com>
2025-06-23 11:49:02 +0900

    feat : 내 정보 확인 수정

--- scripts/migratePlanId.js
+++ scripts/migratePlanId.js
@@ -1,6 +1,7 @@
+// scripts/migratePlanId.js
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import User from '../models/User.js' // scripts 폴더에서 models 폴더로 상대경로 맞춤
+import User from '../models/User.js'
 import Plan from '../models/Plan.js'
 
 dotenv.config()
@@ -10,15 +11,22 @@ async function migrate() {
     await mongoose.connect(process.env.MONGO_URI)
     console.log('DB connected')
 
-    const users = await User.find({ plan_id: { $exists: false } })
+    // plan_id 필드가 없거나 null인 유저 찾기
+    const users = await User.find({ plan_id: { $exists: false } }).lean()
 
     for (const user of users) {
       if (user.plan) {
+        // plan 필드 값으로 Plan 컬렉션에서 문서 조회
         const planDoc = await Plan.findOne({ plan_name: user.plan })
         if (planDoc) {
-          user.plan_id = planDoc._id
-          user.plan = undefined // 필요하면 제거
-          await user.save()
+          // plan_id 업데이트 + plan 필드 완전 삭제
+          await User.updateOne(
+            { _id: user._id },
+            {
+              $set: { plan_id: planDoc._id },
+              $unset: { plan: '' },
+            }
+          )
           console.log(`User ${user.email} updated with plan_id ${planDoc._id}`)
         } else {
           console.log(`Plan not found for user ${user.email}, plan name: ${user.plan}`)
