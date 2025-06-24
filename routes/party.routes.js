ce66620ad4998811003ba74134d6c8df7d23eced
박용규 <yoyo5411@naver.com>
2025-06-24 23:56:34 +0900

    feat : Party 생성/탈퇴 기능

--- routes/party.routes.js
+++ routes/party.routes.js
@@ -1,19 +1,30 @@
 import express from 'express'
 import auth from '../middleware/authMiddleware.js'
 import {
-  getPartyStatus,
   getMyPartyInfo,
-  leaveParty,
-  checkMatchStatus,
-  executePartyMatch,
+  handleApplicationByRole,
+  handlePartyLeave,
+
+  // getPartyStatus,
+  // leaveParty,
+  // checkMatchStatus,
+  // executePartyMatch,
 } from '../functions/partyFunctions.js'
 
 const router = express.Router()
 
-router.get('/status', auth, getPartyStatus)
+//파티 가입 정보 가져오기
 router.get('/infor', auth, getMyPartyInfo)
-router.post('/leave', auth, leaveParty)
-router.get('/match', auth, checkMatchStatus)
-router.post('/match', auth, executePartyMatch)
+
+//body 에 role을 받고, 처리
+router.post('/apply', auth, handleApplicationByRole)
+
+//파티 탈퇴
+router.post('/leave', auth, handlePartyLeave)
+
+// router.get('/status', auth, getPartyStatus)
+// router.post('/leave', auth, leaveParty)
+// router.get('/match', auth, checkMatchStatus)
+// router.post('/match', auth, executePartyMatch)
 
 export default router
